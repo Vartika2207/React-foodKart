b87030edafe27a0f436caee1ed44a38d4b3717ec
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-01-10 04:24:42 +0530

    Added react scripts

---
+++ 01-Basic-setup/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,11 @@
+//component responsible to pring actual list of meals to screen
+import React from 'react'
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  return (
+    <div>AvailableMeals</div>
+  )
+};
+
+export default AvailableMeals;
---
+++ 01-Basic-setup/src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+//rendering that meals list, summary text about it
+import React from 'react'
+
+const Meals = () => {
+  return (
+    <div>Meals</div>
+  )
+};
+
+export default Meals;
